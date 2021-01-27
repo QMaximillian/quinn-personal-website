e646f4af1d7a36ec1852c026e234540ded2c1877
“Quinn <quinnlashinsky@gmail.com>
2021-01-27 16:41:58 -0500

    Add new fonts, update _document.js to handle server stylesheets for Styled Components. Add CSS normalization and global styling components. Update eslintrc.json. Allow devDependencies to be imported for prop-types package

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from 'next/head'
+import styled from 'styled-components'
 
+const Heading = styled.h1`
+  background: red;
+  height: 2em;
+  width: 3em;
+`
 export default function Home() {
   return (
     <div>
@@ -8,6 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>Hello</div>
+      <Heading />
     </div>
   )
 }
