e35c7d32a6236abeee36709ec05524fde0ec0400
“Quinn <quinnlashinsky@gmail.com>
2021-03-01 19:04:21 -0500

    Add my name,  button, and skills

--- pages/_app.js
+++ pages/_app.js
@@ -1,67 +1,33 @@
 import PropTypes from 'prop-types'
-import { css, createGlobalStyle, ThemeProvider } from 'styled-components'
-import { Normalize } from '../components/Normalize'
+import '../styles/globals.css'
 
-const globalStyle = css`
-  @font-face {
-    font-family: 'Cera Pro Regular';
-    src: url('/fonts/CeraPRO-Regular.ttf');
-    font-style: normal;
-  }
+// @font-face {
+//   font-family: 'Cera Pro Regular';
+//   src: url('/fonts/CeraPRO-Regular.ttf');
+//   font-style: normal;
+// }
 
-  @font-face {
-    font-family: 'Cera Pro Bold';
-    src: url('/fonts/CeraPRO-Bold.ttf');
-    font-style: bold;
-  }
+// @font-face {
+//   font-family: 'Cera Pro Bold';
+//   src: url('/fonts/CeraPRO-Bold.ttf');
+//   font-style: bold;
+// }
 
-  @font-face {
-    font-family: 'Cera Pro Medium';
-    src: url('/fonts/CeraPRO-Medium.ttf');
-    font-style: medium;
-  }
+// @font-face {
+//   font-family: 'Cera Pro Medium';
+//   src: url('/fonts/CeraPRO-Medium.ttf');
+//   font-style: medium;
+// }
 
-  body {
-    font-family: Cera Pro Regular;
-  }
-`
-
-const GlobalStyle = createGlobalStyle(globalStyle)
-GlobalStyle.displayName = 'GlobalStyle'
-
-const theme = {
-  // Colors
-  colors: {
-    'un-blue': '#5386e4',
-    'middle-blue-green': '#77cbb9',
-    xiketic: '#220C10',
-    snow: '#fcf7f8',
-  },
-  // Fonts
-  fonts: {},
-  fontSizes: {
-    sm: '.5em',
-    md: '.75em',
-    normal: '1em',
-    lg: '1.25em',
-    xl: '1.5em',
-    '2xl': '2em',
-  },
-  breakpoints: {
-    tablet: '43em',
-    desktop: '62em',
-    'desktop-xl': '82em',
-  },
-}
+// body {
+//   font-family: Cera Pro Regular;
+//   height: 100vh;
+// }
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Normalize />
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,45 @@
 import Head from 'next/head'
-import styled from 'styled-components'
+import Footer from '../components/Footer'
+import skills from '../skills.json'
 
-const Heading = styled.h1`
-  background: red;
-  height: 2em;
-  width: 3em;
-`
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title>Homepage</title>
+        <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Hello</div>
-      <Heading />
-    </div>
+      <main className="h-full w-full flex flex-col justify-start items-center bg-black">
+        <div className=" text-white font-semibold  rounded-sm flex flex-col justify-center items-start h-full w-10/12">
+          <div>
+            <div className="text-green-300 text-6xl">I&apos;m Quinn</div>
+            <div className="text-lg">Web Developer • Runner • Baseball</div>
+          </div>
+          <button type="button" className="mt-2 p-1 text-base border">
+            Contact Me
+          </button>
+          <div className="mt-10">
+            {skills.map((skill) => {
+              return (
+                <div>
+                  <div>{skill.name}</div>
+                  <div>
+                    {skill.frameworks?.map((framework, i) => (
+                      <span className="text-yellow-500 first:pl-2">
+                        {framework}
+                        <span className="mx-1">
+                          {i !== skill.frameworks.length - 1 && `•`}
+                        </span>
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        {/* <Footer /> */}
+      </main>
+    </>
   )
 }
