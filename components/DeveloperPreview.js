7939b8c065fc00be278bcc28f0e0429d9a971f0a
“Quinn <quinnlashinsky@gmail.com>
2021-03-14 21:50:23 -0400

    Adjust  and  styling

--- components/DeveloperPreview.js
+++ components/DeveloperPreview.js
@@ -3,25 +3,30 @@ import skills from '../skills.json'
 
 function DeveloperPreview() {
   return (
-    <div className="tracking-wide text-white font-bold flex flex-col justify-center items-start h-full">
-      <div>
-        <div className="text-green-300 text-7xl">I&apos;m Quinn</div>
-        <div className="text-2xl font-semibold">
+    <div className="w-full tracking-wide text-white font-bold flex flex-col justify-center items-start h-full">
+      <div className="w-full">
+        <div className="text-green-300 text-6xl md:text-7xl">
+          I&apos;m Quinn
+        </div>
+        <div className="text-xl md:text-2xl font-semibold">
           Web Developer • Runner • Baseball
         </div>
         <button
           type="button"
-          className="rounded-sm py-2 px-8 mt-6 min-w-min max-w-xs text-lg text-center border-2 hover:text-green-300 hover:border-green-300 focus:text-green-300 focus:border-green-300 active:text-green-300 active:border-green-300"
+          className="w-full sm:w-60 rounded-sm py-2 mt-6 sm:min-w-min sm:max-w-xs text-md md:text-lg text-center border-2 hover:text-green-300 hover:border-green-300 focus:text-green-300 focus:border-green-300 active:text-green-300 active:border-green-300"
         >
           Contact Me
         </button>
       </div>
-      <div className="mt-5">
+      <div className="mt-5 w-full">
+        <div className="text-2xl md:text-4xl text-green-300 underline">
+          Skills
+        </div>
         {skills.map((skill) => {
           return (
-            <div className="text-2xl" key={skill.name}>
-              <div className="pt-1">{skill.name}</div>
-              <div className="pt-1">
+            <div className="text-lg sm:text-2xl" key={skill.name}>
+              <div className="">{skill.name}</div>
+              <div className="">
                 {skill.frameworks?.map((framework, i) => (
                   <span
                     key={framework}
--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,15 @@ export default function Home() {
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="h-full w-full flex flex-col md:flex-row justify-start items-center bg-black p-8">
-        <div className=" w-10/12 md:w-1/2 min-h-full md:mr-2 flex items-center">
-          <DeveloperPreview />
-        </div>
-        {/* Placeholder Component 
+      <main className="h-full w-full flex flex-col md:flex-row justify-start items-center bg-black md:p-8">
+        <div className="h-full justify-center">
+          <div className="w-full sm:w-10/12 md:w-1/2 min-h-full md:mr-2 flex items-center px-4">
+            <DeveloperPreview />
+          </div>
+          {/* Placeholder Component 
         <div className="w-1/2 h-full border border-red-500 ml-2 hidden md:block" />
         */}
+        </div>
       </main>
     </>
   )
