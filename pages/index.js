8d88c4c9cd2418c565e2fdbffbb63b6e6f8eeb6a
“Quinn <quinnlashinsky@gmail.com>
2021-03-15 20:01:06 -0400

    Adjust  styling to  component

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,13 @@ export default function Home() {
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="h-full w-full flex flex-col md:flex-row justify-start items-center bg-black md:p-8">
-        <div className="h-full justify-center">
-          <div className="w-full sm:w-10/12 min-h-full md:mr-2 flex items-center px-4">
+      <main className="h-full w-full flex flex-col md:flex-row justify-start lg:justify-center items-center bg-black md:p-8">
+        <div className="h-full flex justify-center lg:w-3/4">
+          <div className="w-full sm:w-5/6 lg:w-1/2 min-h-full md:mr-2 flex items-center px-4">
             <DeveloperPreview />
           </div>
-          {/* Placeholder Component 
-        <div className="w-1/2 h-full border border-red-500 ml-2 hidden md:block" />
-        */}
+          {/* Placeholder Component */}
+          {/* <div className="w-1/2 h-full border border-red-500 ml-2 hidden lg:block" /> */}
         </div>
       </main>
     </>
