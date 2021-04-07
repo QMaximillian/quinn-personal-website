ac84c7fecc2c1b1af11ea3a37a489834b358cf15
“Quinn <quinnlashinsky@gmail.com>
2021-04-07 19:03:02 -0400

    Add a navigation link to send a user back to the home page

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,14 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import Link from 'next/link'
 import * as React from 'react'
 import NavigationLink from './NavigationLink'
 
 function Navigation() {
   return (
-    <nav className="flex w-full text-xs sm:text-sm border-2 border-black">
-      <NavigationLink href="/#projects" text="Projects" />
-      <NavigationLink href="/#contact" text="Contact" />
-      <NavigationLink href="/#me" text="Me" />
-      <NavigationLink href="/#skills" text="Skills" />
+    <nav className="w-full text-xs sm:text-sm border border-black primary-gradient">
+      <div className="w-full flex">
+        <NavigationLink href="/projects" text="Projects" />
+        <NavigationLink href="/contact" text="Contact" />
+        <NavigationLink href="/me" text="Me" />
+        <NavigationLink href="/skills" text="Skills" />
+      </div>
+      <Link href="/">
+        <a className="inline-block w-full text-center h-8 py-2 border-t border-black">
+          The Rundown
+        </a>
+      </Link>
     </nav>
   )
 }
