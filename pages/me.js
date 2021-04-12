dcea1e145abf71135b7fce1101497367454a3796
“Quinn <quinnlashinsky@gmail.com>
2021-04-12 14:48:21 -0400

    Add max width for '/me' route

--- pages/me.js
+++ pages/me.js
@@ -3,14 +3,14 @@ import Contact from '../components/Contact'
 
 export default function Me() {
   return (
-    <div className="h-full w-full flex flex-col content-end">
-      <div className="max-w-3xl h-full">
+    <>
+      <div className="max-w-3xl  h-full w-full flex flex-col content-end">
         <DeveloperInfo
           imgSrc="images/quinn-with-timmy.jpeg"
           imgAlt="Quinn Lashinsky holding and petting Timmy the cat"
         />
       </div>
       <Contact />
-    </div>
+    </>
   )
 }
