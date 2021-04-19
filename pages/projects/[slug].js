bec8a4f42ec398c49734ed27df7d1c8346a1e824
“Quinn <quinnlashinsky@gmail.com>
2021-04-19 13:46:16 -0400

    Add slug key to projects.json to use in creating a template for showing each project show page

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,5 +1,32 @@
-function ProjectDetail() {}
+function ProjectDetail({ name, image, description }) {
+  return <div />
+}
 
-export async function getStaticProps() {}
+export async function getStaticPaths() {
+  const projectPages = await import('../../projects.json')
 
+  const slugs = projectPages.default.map((projectPage) => projectPage.slug)
+
+  const paths = slugs.map((slug) => ({ params: { slug } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const projectPages = await import('../../projects.json')
+  const projectPage = projectPages.default.find((x) => x.slug === slug)
+
+  return {
+    props: {
+      name: projectPage.name,
+      image: projectPage.image,
+      description: projectPage.description,
+      href: projectPage.href,
+      slug: projectPage.slug,
+    },
+  }
+}
 export default ProjectDetail
