e1483f25914c2d0cecc82737d4c64475cc6e68e6
“Quinn <quinnlashinsky@gmail.com>
2021-04-20 20:54:23 -0400

    Add default props and props-types to Toast component

--- components/Toast.js
+++ components/Toast.js
@@ -1,8 +1,16 @@
 import * as React from 'react'
+import PropTypes from 'prop-types'
 import Alert from '@reach/alert'
 
-function Toast({ message }) {
+function Toast({ message = 'Copied' }) {
   return <Alert type="polite">{message}</Alert>
 }
 
+Toast.propTypes = {
+  message: PropTypes.string,
+}
+
+Toast.defaultProps = {
+  message: 'Copied',
+}
 export default Toast
